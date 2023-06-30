3507ac9deb8a1f63bbaf6536b2e9bd9444d842ed
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-06-30 10:42:24 -0300

    UPDATE: style components and click button filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,93 @@
+"use client";
+
 import Image from "next/image";
 import logo from "@/styles/assets/logo.png";
+import backgroundTitle from "@/styles/assets/background-title.png";
 import styled from "styled-components";
 
-const CardRounded = styled.p`
-  padding: "10px 12px";
-  border-radius: 20px 20px 20px 4px;
+const Header = styled.header`
+  padding: 44px;
 `;
 
-export default function Home() {
+const Rounded = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #1c3c50;
+  border-radius: 30px;
+  padding: 6px 18px;
+  font-size: 16px;
+  font-weight: 400;
+  text-center;
+
+  &:hover {
+    color: #007eff;
+    border: 1px solid #007eff;
+  }
+`;
+
+const FilterList = styled.div``;
+
+const SelectFilter = styled.select`
+  border: 1px solid #1c3c50;
+  color: #1c3c50;
+  border-radius: 8px;
+  padding: 8px 12px;
+  font-size: 16px;
+
+  &:active,
+  &:focus-visible {
+    border: 1px solid #007eff !important;
+    color: #007eff !important;
+  }
+`;
+
+const Hr = styled.div`
+  border: 1px solid #ccc;
+  width: 60%;
+`;
+export default function Home(): JSX.Element {
+  function buttonFilter(e: React.MouseEvent<HTMLButtonElement>) {
+    // console.log(e.target.parentElement);
+
+    // console.log(childrensList);
+    e.target?.classList.add("active");
+  }
+
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-4">
-        <header>
-          <Image src={logo} alt="logo" width={120} height={220} />
-        </header>
-        <section className="" style={{ padding: "178px" }}>
-          <CardRounded>WEBINAR EXCLUSIVO</CardRounded>
-          <p>Menos Conversinha</p>
-          <hr />
-          <p>Mais conversão</p>
-          <p>
-            Conheça as estratégias que mudaram o jogo e como aplicá-las no seu
-            negócio
-          </p>
-        </section>
-        Olá
+      <main className="flex flex-col items-center justify-between p-4">
+        <Header>
+          <Image src={logo} alt="logo" width={200} height={440} />
+        </Header>
+
+        <Image
+          src={backgroundTitle}
+          alt="background"
+          style={{ width: "100%", marginBottom: "100px" }}
+        />
+        <FilterList className="flex justify-center items-center gap-24 pb-6">
+          <div className="flex justify-center items-center gap-5">
+            <Rounded onClick={(e: any) => buttonFilter(e)}>Agências</Rounded>
+            <Rounded onClick={(e: any) => buttonFilter(e)}>Chatbot</Rounded>
+            <Rounded onClick={(e: any) => buttonFilter(e)}>
+              Marketing Digital
+            </Rounded>
+            <Rounded onClick={(e: any) => buttonFilter(e)}>
+              Geração de Leads
+            </Rounded>
+            <Rounded onClick={(e: any) => buttonFilter(e)}>Mídia Paga</Rounded>
+          </div>
+          <div className="flex gap-2 items-center ">
+            <p className="font-bold" style={{ color: "#1c3c50" }}>
+              Ordenar por:
+            </p>
+            <SelectFilter>
+              <option value="0">Data de publicação</option>
+            </SelectFilter>
+          </div>
+        </FilterList>
+        <Hr />
       </main>
     </>
   );
