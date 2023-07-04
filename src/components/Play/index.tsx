7279023799371615f309eb755983d1879dd2813d
Flavio Leonardo Machado <flmp.leonardo@gmail.com>
2023-07-03 23:25:32 -0300

    ADD: Play Component

---
+++ src/components/Play/index.tsx
@@ -0,0 +1,67 @@
+import ButtonsDonwload from "./ButtonsDonwload";
+
+interface IProp {
+  closeModal: () => void;
+}
+
+export default function Play({ closeModal }: IProp): JSX.Element {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50">
+      <div className="bg-gray-900 bg-opacity-40 absolute inset-0"></div>
+
+      <div className="bg-white rounded-2xl w-full max-w-xl z-10 scale-up-center">
+        <Header closeModal={closeModal} />
+
+        <div className="bg-gray-700" style={{ height: "350px" }}>
+          Video
+        </div>
+        <Footer />
+      </div>
+
+      <div className="absolute left-0 bottom-0 bg-white px-4 pb-3 scale-up-center">
+        <ButtonsDonwload className="flex-col" isActiveButton={true} />
+      </div>
+    </div>
+  );
+}
+
+const Header = ({ closeModal }: IProp) => (
+  <div className="px-2">
+    <div
+      className="flex flex-col border-t-4 rounded-md border-blue-500 py-2 "
+      style={{ marginTop: "-2px" }}
+    >
+      <button
+        className="text-gray-600 hover:text-gray-800 self-end"
+        onClick={() => closeModal()}
+      >
+        <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24">
+          <path d="M6.293 6.293a1 1 0 011.414 0L12 10.586l4.293-4.293a1 1 0 011.414 1.414L13.414 12l4.293 4.293a1 1 0 01-1.414 1.414L12 13.414l-4.293 4.293a1 1 0 01-1.414-1.414L10.586 12 6.293 7.707a1 1 0 010-1.414z" />
+        </svg>
+      </button>
+      <h2 className="text-xl font-bold max-w-xs self-center mb-6">
+        <strong style={{ color: "#0085ff" }}>Webinar:</strong> Como aumentar sua
+        Gerção de Leads feat. Traktor
+      </h2>
+    </div>
+  </div>
+);
+
+const Footer = () => (
+  <div className="px-6 py-4 pb-7 bg-gray-100 text-xs rounded-b-2xl">
+    <div className="flex flex-col">
+      <p className="border-b-2 border-gray-200 py-3">
+        <strong>Descrição</strong>
+      </p>
+      <p className="pt-3 pb-4">
+        Lorem ipsum dolor sit amet, consectetur adiping elit. Morbi eu cursus
+        massa. Etiam efficitur est at dolor et sollicitudin ullamcorper at et
+        enim. Suspendisse eleifend metus ultrices iaculis aliquam.
+      </p>
+    </div>
+    <div className="flex flex-col">
+      <h3 className="font-bold border-b-2 border-gray-200 py-3">Download</h3>
+      <ButtonsDonwload />
+    </div>
+  </div>
+);
